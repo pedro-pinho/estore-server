5c46f713257e2718dbedf3379454b8da670f1510
Pedro dos Santos Pinho <pedro.dossantos@inbrace.com>
2024-06-28 18:24:28 -0300

    User Login Method

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const pool = require('../shared/pool');
 const bcryptjs = require('bcryptjs');
+const jwtoken = require('jsonwebtoken');
 const usersApp = express.Router();
 
 usersApp.get('/', (req, res) => {
@@ -42,6 +44,38 @@ usersApp.post('/signup', (req, res) => {
     });
   } catch (error) {
     res.status(500).send({
+      error: error.code,
+      message: error.message
+    });
+  }
+});
+
+usersApp.post('/login', (req, res) => {
+  try {
+    const { email, password } = req.body;
+    pool.query('SELECT * FROM users WHERE email = ?', email, async (error, results) => {
+      if (error) {
+        res.status(500).send({
+          error: error.code,
+          message: error.message
+        });
+      } else {
+        if (results.length > 0) {
+          const user = results[0];
+          const passwordMatch = await bcryptjs.compare(password, user.password);
+          if (passwordMatch) {
+            const token = jwtoken.sign({ email: user.email, id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            res.status(200).send({ token, expiresIn: 3600});
+          } else {
+            res.status(401).send({ error: 401, message: 'Invalid credentials' });
+          }
+        } else {
+          res.status(401).send({ error: 401, message: 'Invalid credentials' });
+        }
+      }
+    });
+  } catch (error) {
+    res.status(400).send({
       error: error.code,
       message: error.message
     });
